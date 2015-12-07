b7b90e1be9526160085ae3f4af105e40dd82b927
cvidler <christopher.vidler@dynatrace.com>
2015-12-07 13:52:33 +1100

    debug code

--- archivemgmt.sh
+++ archivemgmt.sh
@@ -9,6 +9,12 @@
 # Config
 BASEDIR=/var/spool/rtmarchive
 SCRIPTDIR=~/rtmarchive
+
+
+
+# Script below do not edit
+set -euo pipefail
+IFS=$',\n\t'
 DEBUG=${1:-0}
 AWK=`which awk`
 CAT=`which cat`
@@ -17,13 +23,8 @@ BZIP2=`which bzip2`
 DATE=`which date`
 MD5SUM=`which md5sum`
 
-#DEBUG=1
 
 
-
-# Script below do not edit
-set -euo pipefail
-IFS=$',\n\t'
 
 function debugecho {
         if [ $DEBUG -ne 0 ]; then echo -e "$@"; fi
@@ -47,50 +48,56 @@ for DIR in "$BASEDIR"/*; do
 			if [ ! -d "$MONTH" ]; then continue; fi
 			for DAY in "$MONTH"/*;  do
 				if [ ! -d "$DAY" ]; then continue; fi
-				debugecho "$DAY"
+				debugecho "***DEBUG: Processing directory $DAY"
 				DATADATE=`echo $YEAR | $AWK ' match($0,"(.+/)+(.+)$",a) { print a[2] } ' `-`echo $MONTH | $AWK ' match($0,"(.+/)+(.+)$",a) { print a[2] } ' `-`echo $DAY | $AWK ' match($0,"(.+/)+(.+)$",a) { print a[2] } ' `
-				debugecho "$DATADATE"
+				debugecho "***DEBUG: Processing date: $DATADATE"
 
 				# process zdata files if found (if not probably archived already)
 				set +e
-				ZCOUNT=$(ls $DAY/zdata_* 2> /dev/null | wc -l)
+				ZCOUNT=$(ls "$DAY"/zdata_* 2> /dev/null | wc -l)
 				set -e
 				updated=0
-				if [ $ZCOUNT -ne 0 ]; then 
+				#if there's zdata (already archived) and it's not todays date (incomplete data), then archvie it
+				if [ $ZCOUNT -ne 0 ]; then if [ `$DATE +%Y%m%d` -gt `$DATE -d $DATADATE +%Y%m%d` ]; then
 					for ZDATA in $DAY/zdata_*; do
-						debugecho $ZDATA
-						$AWK -F" " '$1=="U" { a[$7]++ } END { for (b in a) {print b} }' $ZDATA >> $DAY/softwareservice.lst.tmp
-						$AWK -F" " '$1=="U" { a[$2]++ } END { for (b in a) {print b} }' $ZDATA >> $DAY/serverips.lst.tmp
-						$AWK -F" " '$1=="U" { a[$3]++ } END { for (b in a) {print b} }' $ZDATA >> $DAY/clientips.lst.tmp
-						$AWK -F" " '$1=="U" { a[$6]++ } END { for (b in a) {print b} }' $ZDATA >> $DAY/serverports.lst.tmp
+						debugecho "***DEBUG: Processing: $ZDATA"
+						$AWK -F" " '$1=="#TS:" { print $2","strftime("%c",strtonum("0x"$2),1); }' "$ZDATA" >> "$DAY"/timestamps.lst.tmp
+						$AWK -F" " '$1=="U" { a[$7]++ } END { for (b in a) {print b} }' "$ZDATA" | 
+							$AWK -vRS='%[0-9a-fA-F]{2}' 'RT{sub("%","0x",RT);RT=sprintf("%c",strtonum(RT))}{gsub(/\+/," ");printf "%s", $0 RT}' >> "$DAY"/softwareservice.lst.tmp
+						$AWK -F" " '$1=="U" { a[$2]++ } END { for (b in a) {print b} }' "$ZDATA" >> "$DAY"/serverips.lst.tmp
+						$AWK -F" " '$1=="U" { a[$3]++ } END { for (b in a) {print b} }' "$ZDATA" >> "$DAY"/clientips.lst.tmp
+						$AWK -F" " '$1=="U" { a[$6]++ } END { for (b in a) {print b} }' "$ZDATA" >> "$DAY"/serverports.lst.tmp
 						updated=1
 					done
 
 					if [ $updated -ne 0 ]; then
 						# de-dupe data files
-						for file in softwareservice.lst serverips.lst clientips.lst serverports.lst; do
-							$AWK '!seen[$0]++' $DAY/$file.tmp > $DAY/$file
-							rm $DAY/$file.tmp
+						for file in timestamps.lst softwareservice.lst serverips.lst clientips.lst serverports.lst; do
+							$AWK '!seen[$0]++' "$DAY"/$file.tmp > "$DAY"/$file
+							rm "$DAY"/$file.tmp
 						done
 					fi
 
-					# archive it all, if not todays data (assumes incomplete, finish it tomorrow)
-					if [ `$DATE +%Y%m%d` -gt `$DATE -d $DATADATE +%Y%m%d` ]; then 
-						ARCNAME=$AMDNAME-$DATADATE.tar.bz2
-						$TAR -cjf $MONTH/$ARCNAME $DAY/* >&2
-						if [ $? -eq 0 ]; then
-							#succesful
-							$MD5SUM $MONTH/$ARCNAME > $MONTH/$ARCNAME.md5
-							rm -f $DAY/*data_* $DAY/page2transmap_*
-						else
-							#failed
-							echo -e "\e[33m***WARNING:\e[0m Couldn't archive data files in: $DAY, will try again next time."
-						fi
+					# archive it all
+					ARCNAME=$AMDNAME-$DATADATE.tar.bz2
+					$TAR -cjf "$MONTH"/$ARCNAME "$DAY"/* >&2
+					if [ $? -eq 0 ]; then
+						#succesful, checksum the archive and clean up data files
+						$MD5SUM $MONTH/$ARCNAME > $MONTH/$ARCNAME.md5
+						rm -f "$DAY"/*data_* "$DAY"/vdataidx_* "$DAY"/page2transmap_*
 					else
-						debugecho "$DATADATE = today, not archiving"
+						#failed
+						echo -e "\e[33m***WARNING:\e[0m Couldn't archive data files in: $DAY, will try again next time."
 					fi
 				else
-					debugecho "\e[33m***WARNING:\e[0m No zdata files in: $DAY."
+					debugecho "***DEBUG: $DATADATE = today, not archiving."
+				fi
+				else
+					if [ ! -r "$DAY/softwareservice.lst" ]; then
+						debugecho "\e[33m***WARNING:\e[0m No zdata files in: $DAY."
+					else
+						debugecho "***DEBUG: Already archived: $DAY"
+					fi
 				fi
 
 			done
