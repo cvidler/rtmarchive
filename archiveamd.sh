3136164528038400b9ec0847acccf6ab53dc43ae
cvidler <christopher.vidler@dynatrace.com>
2016-05-02 17:24:05 +1000

    initial changes required to support HS AMD (12.4.5+)

--- archiveamd.sh
+++ archiveamd.sh
@@ -12,9 +12,22 @@
 # Config defaults
 DEBUG=0
 
+# zdata file extensions for HS AMD (12.4.5+)
+ZDATAEXTS=db,dns,gen,h,he,ica,ip,mq,of,rmi,sap,smb,unv,voip,vol,xml
+# vdata/vdataidx extensions for HS AMD
+VDATAEXTS=db,h,mq,of,rmi,sap,unv,xml
+# hpdata extensions for HS AMD
+HPDATAEXTS=db,h,ica,mq,of,rmi,sap,unv,xml
+# headerdata exts...
+HDATAEXTS=h,of,xml
+# ndata exts
+NDATAEXTS=rtm
+# amddata exts
+AMDDATAEXTS=rtm,amd
 
 
-# Script below do not edit
+
+# --- Script below do not edit ---
 set -euo pipefail
 IFS=$',\n\t'
 
@@ -159,13 +172,33 @@ for i in 1 2 3; do
 		echo -e "\e[31m***FATAL:\e[0m Can not download directory listing from AMD: $AMDNAME try: ${i}" >&2
 		fail=$((fail + 1))
 	else
-		$CAT $tmpfile | $GUNZIP -c > $AMDDIR/currdir.lst
+		$CAT $tmpfile | $GUNZIP -q -c > $AMDDIR/currdir.lst
 		break
 	fi
 	set -e
 done
 rm $tmpfile
 if [ $fail -ne 0 ]; then echo -e "\e[31m***FATAL:\e[0m Could not download directory listing from AMD: $AMDNAME Aborting." >&2 ; exit 1; fi
+
+
+# test for HS AMD, version commands responds with line "ng.probe=true"
+fail=0
+tmpfile=`$MKTEMP`
+for i in 1 2 3; do
+	set +e
+	$WGET --quiet --no-check-certificate -O "$tmpfile" $URL/RtmDataServlet?cmd=version
+	if [ $? -ne 0 ]; then
+		echo -e "\e[31m***FATAL:\e[0m Can not download version info from AMD: $AMDNAME try: ${i}" >&2
+		fail=$((fail + 1))
+	else
+		HSAMD=`$AWK '/ng\.probe=true/ { print "1" }' "$tmpfile"`
+		break
+	fi
+	set -e
+done
+rm $tmpfile
+if [ $fail -ne 0 ]; then echo -e "\e[31m***FATAL:\e[0m Could not download version info from AMD: $AMDNAME Aborting." >&2 ; exit 1; fi
+debugecho "HS AMD detected: $HSAMD"
 
 
 downloaded=0
@@ -189,58 +222,96 @@ while read p; do
 		fi
 		file=$ARCDIR/${p}
 
-		if [ -r "$file" ]; then continue; fi	# already exists skip downloading
+		# HS AMD produces many zdata files split, these are NOT listed in the downloaded directory listing, only the base names - yay :(
+		# we must handle this. 
+		filelist=""
+		if [ $HSAMD ] && [ -z "${p##zdata_*}" ]; then
+			# build required filenames to download
+			#debugecho $ZDATAEXTS
+			for e in $ZDATAEXTS; do
+				filelist=${filelist}${p}_${e},
+			done
+		elif [ $HSAMD ] && ( [ -z "${p##vdata_*}" ] || [ -z "${p##vdataidx_*}" ] ); then
+			for e in $VDATAEXTS; do
+				filelist=${filelist}${p}_${e},
+			done
+		elif [ $HSAMD ] &&  [ -z "${p##hpdata_*}" ]; then
+			for e in $HPDATAEXTS; do
+				filelist=${filelist}${p}_${e},
+			done
+		elif [ $HSAMD ] && [ -z "${p##headerdata_*}" ]; then
+			for e in $HDATAEXTS; do
+				filelist=${filelist}${p}_${e},
+			done
+		elif [ $HSAMD ] && [ -z "${p##ndata_*}" ]; then
+			for e in $NDATAEXTS; do
+				filelist=${filelist}${p}_${e},
+			done
+		elif [ $HSAMD ] && [ -z "${p##amddata_*}" ]; then
+			for e in $AMDDATAEXTS; do
+				filelist=${filelist}${p}_${e},
+			done
+		else
+			filelist=${p}
+		fi
+		debugecho "filelist: $filelist"
 
-	        debugecho "***DEBUG: Downloading ${p} from $AMDNAME to $file"
-		# Try download 3 times
-		warn=0
-		tmpfile=`$MKTEMP`
-		for i in 1 2 3 ; do
-			set +e
-			$WGET --quiet --no-check-certificate -O "$tmpfile" $URL/RtmDataServlet?cmd=zip_entry\&entry=${p}
-			if [ $? -ne 0 ]; then
-				warn=$((warn + 1))
-	        		debugecho "\e[33m***WARNING:\e[39m Can not download file: ${p} from AMD: $AMDNAME try: ${i}" >&2
-			else
-				$CAT $tmpfile | $GUNZIP -c > $file
-				downloaded=$((downloaded + 1))
-				break
-			fi
-			set -e
-		done
-		rm $tmpfile
-		if [ $warn -ne 0 ]; then echo -e "\e[33m***WARNING:\e[39m Could not download file: ${p} from AMD: $AMDNAME" >&2 ; warnings=$((warnings + 1)); fi
+		for f in $filelist; do
+			file=$ARCDIR/${f}
+
+			if [ -r "$file" ]; then continue; fi	# already exists skip downloading
+
+			debugecho "***DEBUG: Downloading ${f} from $AMDNAME to $file"
+			# Try download 3 times
+			warn=0
+			tmpfile=`$MKTEMP`
+			for i in 1 2 3 ; do
+				set +e
+				$WGET --quiet --no-check-certificate -O "$tmpfile" $URL/RtmDataServlet?cmd=zip_entry\&entry=${f}
+				if [ $? -ne 0 ]; then
+					warn=$((warn + 1))
+			    		debugecho "\e[33m***WARNING:\e[39m Can not download file: ${f} from AMD: $AMDNAME try: ${i}" >&2
+				else
+					$CAT "$tmpfile" | $GUNZIP -q -c > "$file"
+					downloaded=$((downloaded + 1))
+					break
+				fi
+				set -e
+			done
+			rm $tmpfile
+			if [ $warn -ne 0 ]; then echo -e "\e[33m***WARNING:\e[39m Could not download file: ${f} from AMD: $AMDNAME" >&2 ; warnings=$((warnings + 1)); fi
 	
-		# Set file timestamp correctly
-		# extract timestamp from file name and convert it to require format CCYYMMDDhhmm.SS
-		FTS=`echo ${p} | $AWK -F"_" ' { print strftime("%Y%m%d%H%M.%S",strtonum("0x"$2),1); } '`
-		`TZ=UTC $TOUCH -c -t $FTS  "$file"`
+			# Set file timestamp correctly
+			# extract timestamp from file name and convert it to require format CCYYMMDDhhmm.SS
+			FTS=`echo ${f} | $AWK -F"_" ' { print strftime("%Y%m%d%H%M.%S",strtonum("0x"$2),1); } '`
+			`TZ=UTC $TOUCH -c -t $FTS  "$file"`
 	
-		# Proces contents here
-		if [[ ${p} =~ zdata_.* ]]; then
-			# get UUID from zdata, use to check if the AMD changes unexpectedly.
-			UUID=`$AWK -F" " '$1=="#AmdUUID:" { print $2 }' $file`
-			if [ ! -f "$AMDDIR/uuid.lst" ]; then
-				echo $UUID > $AMDDIR/uuid.lst
-			else
-				while read k; do
-					OLDUUID=${k}
-				done < $AMDDIR/uuid.lst
-				if [ ! "$OLDUUID" == "$UUID" ]; then
-					echo -e "\e[33m***WARNING:\e[39m UUID Mismatch on AMD: $AMDNAME, Old: $OLDUUID, New: $UUID" >&2
-					echo -e "\e[33m***WARNING:\e[39m If this is expected remove file $AMDDIR/uuid.lst to clear the error" >&2
+			# Proces contents here
+			if [[ ${f} =~ "zdata_.*_vol" ]]; then
+				# get UUID from zdata, use to check if the AMD changes unexpectedly.
+				UUID=`$AWK -F" " '$1=="#AmdUUID:" { print $2 }' "$file"`
+				if [ ! -f "$AMDDIR/uuid.lst" ]; then
+					echo $UUID > "$AMDDIR/uuid.lst"
+				else
+					while read k; do
+						OLDUUID=${k}
+					done < "$AMDDIR/uuid.lst"
+					if [ ! "$OLDUUID" == "$UUID" ]; then
+						echo -e "\e[33m***WARNING:\e[39m UUID Mismatch on AMD: $AMDNAME, Old: $OLDUUID, New: $UUID" >&2
+						echo -e "\e[33m***WARNING:\e[39m If this is expected remove file $AMDDIR/uuid.lst to clear the error" >&2
+					fi
 				fi
 			fi
-		fi
+		done
 	else
 		debugecho "\e[33m***WARNING:\e[39m Unknown file: ${p} on AMD: $AMDNAME" >&2
 	fi
-done < <($AWK 'NR==FNR{a[$1]++;next;}!($0 in a)' $AMDDIR/prevdir.lst $AMDDIR/currdir.lst)
+done < <($AWK 'NR==FNR{a[$1]++;next;}!($0 in a)' "$AMDDIR/prevdir.lst" "$AMDDIR/currdir.lst")
 
 
 # finally, make the current dir list the previous one.
-rm $AMDDIR/prevdir.lst
-mv $AMDDIR/currdir.lst $AMDDIR/prevdir.lst
+rm "$AMDDIR/prevdir.lst"
+mv "$AMDDIR/currdir.lst" "$AMDDIR/prevdir.lst"
 
 
 
@@ -299,7 +370,7 @@ while read p; do
 				`TZ=UTC $TOUCH -c -t $FTS "$file"`
 			else
 				downloaded=$((downloaded + 1))
-				$CAT $tmpfile | $GUNZIP -c > $file
+				$CAT $tmpfile | $GUNZIP -q -c > $file
 				TS=`$CAT $file | $HEAD -n 1`
 				TS=$((TS / 1000))
 				FTS=`TZ=UTC $DATE -u -d @$TS +%Y%m%d%H%M.%S`
