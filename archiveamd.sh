661d34c5c8d2b7b1e4be8d3a54b7dbd7722225f7
cvidler <cvidler@users.noreply.github.com>
2016-08-15 17:39:31 +1000

    added optimisations and GA AMD HS support

--- archiveamd.sh
+++ archiveamd.sh
@@ -11,19 +11,8 @@
 
 # Config defaults
 DEBUG=0
-
-# zdata file extensions for HS AMD (12.4.5+)
-ZDATAEXTS=db,dns,gen,h,he,ica,ip,mq,of,rmi,sap,smb,unv,voip,vol,xml
-# vdata/vdataidx extensions for HS AMD
-VDATAEXTS=db,h,mq,of,rmi,sap,unv,xml
-# hpdata extensions for HS AMD
-HPDATAEXTS=db,h,ica,mq,of,rmi,sap,unv,xml
-# headerdata exts...
-HDATAEXTS=h,of,xml
-# ndata exts
-NDATAEXTS=rtm
-# amddata exts
-AMDDATAEXTS=rtm,amd
+DISPCOUNT=250					#print progress bar every x files processed
+DISPCOLS=${COLUMNS:-132}		#default character screen width, read from environment, default to 132
 
 
 
@@ -31,11 +20,6 @@ AMDDATAEXTS=rtm,amd
 set -euo pipefail
 IFS=$',\n\t'
 
-#AMDNAME=${1:-}
-#URL=${2:-}
-#BASEDIR=${3:-}
-#DEBUG=${4:-0}
-#AMDDIR=$BASEDIR/$AMDNAME
 AWK=`which awk`
 WGET=`which wget`
 GUNZIP=`which gunzip`
@@ -45,6 +29,11 @@ CAT=`which cat`
 TAIL=`which tail`
 HEAD=`which head`
 DATE=`which date`
+TR=`which tr`
+
+
+#DISPCOLS used to draw progress bars, resize to acount for screenwidth and extras
+DISPCOLS=$((DISPCOLS - 2 - 5))  #make room for end tags and percentage display
 
 function test {
 	set +e
@@ -160,25 +149,26 @@ fi
 # check for existing current data file list (there shouldn't be one if the script works, remove it)
 if [ -f "$AMDDIR/currdir.lst" ]; then
 	debugecho "Found stale currdidr.lst. Removing it."
-	rm "$AMDDIR/currdir.lst"
+	rm -f "$AMDDIR/currdir.lst"
 fi
 
 # Get data file listing from AMD, try 3 times, abort (FATAL) if failed
 fail=0
 tmpfile=`$MKTEMP`
 for i in 1 2 3; do
-	set +e
+	set +e +x
 	$WGET --quiet --no-check-certificate -O "$tmpfile" $URL/RtmDataServlet?cmd=zip_dir
 	if [ $? -ne 0 ]; then
 		echo -e "\e[33m***WARNING:\e[0m Can not download directory listing from AMD: $AMDNAME try: ${i}" >&2
 		fail=$((fail + 1))
 	else
-		$CAT $tmpfile | $GUNZIP -q -c > $AMDDIR/currdir.lst
+		#$CAT "$tmpfile" | $GUNZIP -q -c > "$AMDDIR/currdir.lst"
+		$GUNZIP -q -c "$tmpfile" > "$AMDDIR/currdir.lst"
 		break
 	fi
-	set -e
+	set -e -x
 done
-rm $tmpfile
+rm -f "$tmpfile"
 if [ $fail -ne 0 ]; then echo -e "\e[31m***FATAL:\e[0m Could not download directory listing from AMD: $AMDNAME Aborting." >&2 ; exit 1; fi
 
 
@@ -197,65 +187,39 @@ for i in 1 2 3; do
 	fi
 	set -e
 done
-rm $tmpfile
+rm -f $tmpfile
 if [ $fail -ne 0 ]; then echo -e "\e[31m***FATAL:\e[0m Could not download version info from AMD: $AMDNAME Aborting." >&2 ; exit 1; fi
 debugecho "HS AMD detected: $HSAMD"
 
 
 downloaded=0
 warnings=0
+count=0
 
 # determine delta of current file list from previous and download them all.
+difflist=`$AWK 'NR==FNR{a[$1]++;next;}!($0 in a)' "$AMDDIR/prevdir.lst" "$AMDDIR/currdir.lst"`
+diffcount=`echo -e "$difflist" | wc -l`
+debugecho "filecount: [$diffcount]", 2
+debugecho "filelist: [$difflist]", 3
 while read p; do
 
 	# Validate file name is something we want.
-	if [ "`echo ${p} | $AWK ' /[a-z0-9]+_[0-9a-f]+_[150a]+_[tb].*/ '`" == "${p}" ]; then
+	if [ "`echo "${p}" | $AWK ' /[a-z0-9]+_[0-9a-f]+_[150a]+_[tb].*/ '`" == "${p}" ]; then
 	 	# Extract date codes from file name	
-		year=`echo ${p} | $AWK -F"_" ' { print strftime("%Y",strtonum("0x"$2),1); } '`
-		month=`echo ${p} | $AWK -F"_" ' { print strftime("%m",strtonum("0x"$2),1); } '`
-		day=`echo ${p} | $AWK -F"_" ' { print strftime("%d",strtonum("0x"$2),1); } '`
-		#echo ${file},$year,$month,$day 
+		year=`echo "${p}" | $AWK -F"_" ' { print strftime("%Y",strtonum("0x"$2),1); } '`
+		month=`echo "${p}" | $AWK -F"_" ' { print strftime("%m",strtonum("0x"$2),1); } '`
+		day=`echo "${p}" | $AWK -F"_" ' { print strftime("%d",strtonum("0x"$2),1); } '`
+		#debugecho "${file},$year,$month,$day"
 		# Check for correct folder structure - create if needed
 		ARCDIR=$AMDDIR/$year/$month/$day/
 		if [ ! -w "$ARCDIR" ]; then
 			debugecho "Creating archive directory: $ARCDIR"
 			mkdir -p "$ARCDIR"
 		fi
-		file=$ARCDIR/${p}
+		file="$ARCDIR/${p}"
 
-		# HS AMD produces many zdata files split, these are NOT listed in the downloaded directory listing, only the base names - yay :(
-		# we must handle this. 
-		filelist=""
-		if [ $HSAMD == 1 ] && [ -z "${p##zdata_*}" ]; then
-			# build required filenames to download
-			debugecho "[$ZDATAEXTS]", 2
-			for e in $ZDATAEXTS; do
-				filelist=${filelist}${p}_${e},
-			done
-		elif [ $HSAMD == 1 ] && ( [ -z "${p##vdata_*}" ] || [ -z "${p##vdataidx_*}" ] ); then
-			for e in $VDATAEXTS; do
-				filelist=${filelist}${p}_${e},
-			done
-		elif [ $HSAMD == 1 ] &&  [ -z "${p##hpdata_*}" ]; then
-			for e in $HPDATAEXTS; do
-				filelist=${filelist}${p}_${e},
-			done
-		elif [ $HSAMD == 1 ] && [ -z "${p##headerdata_*}" ]; then
-			for e in $HDATAEXTS; do
-				filelist=${filelist}${p}_${e},
-			done
-		elif [ $HSAMD == 1 ] && [ -z "${p##ndata_*}" ]; then
-			for e in $NDATAEXTS; do
-				filelist=${filelist}${p}_${e},
-			done
-		elif [ $HSAMD == 1 ] && [ -z "${p##amddata_*}" ]; then
-			for e in $AMDDATAEXTS; do
-				filelist=${filelist}${p}_${e},
-			done
-		else
-			filelist=${p}
-		fi
-		debugecho "filelist: $filelist", 2
+		filelist=${p}
+		debugecho "filelist: $filelist", 3
 
 		for f in $filelist; do
 			file=$ARCDIR/${f}
@@ -273,22 +237,24 @@ while read p; do
 					warn=$((warn + 1))
 			    		debugecho "\e[33m*** WARNING:\e[39m Can not download file: ${f} from AMD: $AMDNAME try: ${i}" >&2
 				else
-					$CAT "$tmpfile" | $GUNZIP -q -c > "$file"
+					if [ -f "$file" ] && [ ! -w "$file" ]; then chmod +w "$file"; fi		#file read-only fix that
+					#$CAT "$tmpfile" | $GUNZIP -q -c > "$file"
+					$GUNZIP -q -c "$tmpfile" > "$file"
 					downloaded=$((downloaded + 1))
 					break
 				fi
 				set -e
 			done
-			rm $tmpfile
+			rm -f "$tmpfile"
 			if [ $warn -ne 0 ]; then echo -e "\e[33m*** WARNING:\e[39m Could not download file: ${f} from AMD: $AMDNAME" >&2 ; warnings=$((warnings + 1)); fi
 	
 			# Set file timestamp correctly
 			# extract timestamp from file name and convert it to require format CCYYMMDDhhmm.SS
-			FTS=`echo ${f} | $AWK -F"_" ' { print strftime("%Y%m%d%H%M.%S",strtonum("0x"$2),1); } '`
+			FTS=`echo "${f}" | $AWK -F"_" ' { print strftime("%Y%m%d%H%M.%S",strtonum("0x"$2),1); } '`
 			`TZ=UTC $TOUCH -c -t $FTS  "$file"`
 	
 			# Proces contents here
-			debugecho "[$HSAMD] [${f}]", 2
+			debugecho "HSAMD: [$HSAMD] f: [${f}]", 2
 			extract=0
 			if [[ ${f} =~ ^zdata_.*_t_vol$ ]]; then
 				extract=1
@@ -297,13 +263,13 @@ while read p; do
 			else
 				extract=0
 			fi 
-			debugecho "[$extract]", 2
-			if [ $extract -ne 0 ]; then
+			debugecho "extract: [$extract]", 2
+			if [ $extract -ne 0 ] && [ -r "$file" ]; then
 				# get UUID from zdata, use to check if the AMD changes unexpectedly.
 				debugecho "Extracting UUID from zdata file: ${f}"
 				UUID=`$AWK -F" " '$1=="#AmdUUID:" { print $2 }' "$file"`
 				if [ ! -f "$AMDDIR/uuid.lst" ]; then
-					echo $UUID > "$AMDDIR/uuid.lst"
+					echo "$UUID" > "$AMDDIR/uuid.lst"
 				else
 					while read k; do
 						OLDUUID=${k}
@@ -318,11 +284,23 @@ while read p; do
 	else
 		debugecho "\e[33m*** WARNING:\e[39m Unknown file: ${p} on AMD: $AMDNAME" >&2
 	fi
-done < <($AWK 'NR==FNR{a[$1]++;next;}!($0 in a)' "$AMDDIR/prevdir.lst" "$AMDDIR/currdir.lst")
+	count=$((count+1))
+	if ! (( count % $DISPCOUNT )) ; then 		#status update every DISPCOUNT files
+		# figure out percentage
+		PERC=0$(bc -l <<<  "(($count/$diffcount) * 100); " ); PERC=${PERC%.*}; PERC=${PERC#0}; if [ "$PERC" == "" ]; then PERC=0; fi
+		# figure out progress bar length
+		BARL=0$(bc -l <<<  "(((($count/$diffcount) * $DISPCOLS)) / $DISPCOLS) * $DISPCOLS; "); BARL=${BARL%.*}; BARL=${BARL#0}; if [ "$BARL" == "" ]; then BARL=0; fi
+		# figure out progress bar blank length
+		BARR=$((DISPCOLS - BARL)); if [[ $BARR -lt 1 ]]; then BARR=0; fi
+		debugecho "count: [$count], diffcount: [$diffcount], PERC: [$PERC], BARL: [$BARL], BARR: [$BARR], COLUMNS: [$DISPCOLS]", 2
+		echo -e "Processed files from AMD: $AMDNAME $count/$diffcount $PERC%" 
+		echo -e "[`$HEAD -c $BARL < /dev/zero | $TR '\0' '#' ``$HEAD -c $BARR < /dev/zero | $TR '\0' ' '`] $PERC%"
+	fi
+done < <(echo -e "$difflist")
 
 
 # finally, make the current dir list the previous one.
-rm "$AMDDIR/prevdir.lst"
+rm -f "$AMDDIR/prevdir.lst"
 mv "$AMDDIR/currdir.lst" "$AMDDIR/prevdir.lst"
 
 
@@ -331,7 +309,7 @@ mv "$AMDDIR/currdir.lst" "$AMDDIR/prevdir.lst"
 
 # get config
 echo -e "Archiving AMD Config: $AMDNAME"
-# Get data file listing from AMD, try 3 times, abort (FATAL) if failed
+# Get config file listing from AMD, try 3 times, abort (FATAL) if failed
 fail=0
 tmpfile=`$MKTEMP`
 for i in 1 2 3; do
@@ -341,12 +319,13 @@ for i in 1 2 3; do
 		echo -e "\e[33m*** WARNING:\e[0m Can not download config directory listing from AMD: $AMDNAME try: ${i}" >&2
 		fail=$((fail + 1))
 	else
-		$CAT $tmpfile > $AMDDIR/confdir.lst
+		#$CAT $tmpfile > $AMDDIR/confdir.lst
+		cp -f "$tmpfile" "$AMDDIR/confdir.lst"
 		break
 	fi
 	set -e
 done
-rm $tmpfile
+rm -f "$tmpfile"
 if [ $fail -ne 0 ]; then echo -e "\e[31m*** FATAL:\e[0m Could not download config directory listing from AMD: $AMDNAME Aborting." >&2 ; exit 1; fi
 
 # read current file list and download them all.
@@ -361,7 +340,7 @@ while read p; do
 			debugecho "Creating config archive directory: $ARCDIR"
 			mkdir -p "$ARCDIR"
 		fi
-		file=$ARCDIR/$p
+		file="$ARCDIR/$p"
 		
 		#if [ -r "$file" ]; then continue; fi    # already exists skip downloading
 		
@@ -382,6 +361,7 @@ while read p; do
 				`TZ=UTC $TOUCH -c -t $FTS "$file"`
 			else
 				downloaded=$((downloaded + 1))
+				if [ ! -w $file ]; then chmod +w $file; fi		#file read-only fix that
 				$CAT $tmpfile | $GUNZIP -q -c > $file
 				TS=`$CAT $file | $HEAD -n 1`
 				TS=$((TS / 1000))
@@ -391,13 +371,13 @@ while read p; do
 			fi
 			set -e
 		done
-		rm $tmpfile
+		rm -f "$tmpfile"
 																		 
 		#if [ $warn -ne 0 ]; then echo -e "\e[33m*** WARNING:\e[39m Could not download config file: $p from AMD: $AMDNAME" >&2 ; warnings=$((warnings + 1)); fi
 	else
 		debugecho "\e[33m*** WARNING:\e[39m Unknown config file: $p on AMD: $AMDNAME" >&2
 	fi
-done < <($CAT $AMDDIR/confdir.lst)
+done < <($CAT "$AMDDIR/confdir.lst")
 
 
 
